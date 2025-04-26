281fb8f1465c109856b4e227c71b78783d99eff7
psih <oleksandr.duka@2ops.io>
2025-04-26 17:02:07 +0300

    add

---
+++ install.ps1
@@ -0,0 +1,13 @@
+Install-App -PackageName "googlechrome"
+
+# OpenVPN
+Install-App -PackageName "openvpn"
+
+# Azure VPN Client
+Install-App -PackageName "azure-vpn-client"
+
+# Microsoft Teams Machine-Wide Installer
+Install-App -PackageName "teams"
+
+# Notepad++
+Install-App -PackageName "notepadplusplus"
