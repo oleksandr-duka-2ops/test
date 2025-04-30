2d501401818431f1d01ae51e80025cf447ebf90f
oleksandr-duka-2ops <oleksandr.duka@2ops.io>
2025-04-30 14:38:49 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install microsoft-teams -y
+choco install microsoft-teams -y --force
 choco install googlechrome -y --ignore-checksums
 choco install notepadplusplus -y
 choco install openvpn -y
