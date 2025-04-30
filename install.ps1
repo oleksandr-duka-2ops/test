6adc9236bdddb70cc4a9974b5b848a11868fb2eb
oleksandr-duka-2ops <oleksandr.duka@2ops.io>
2025-04-30 13:52:15 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,3 +3,12 @@ choco install microsoft-teams -y
 choco install googlechrome -y --ignore-checksums
 choco install notepadplusplus -y
 choco install openvpn -y
+
+$vpnExeUrl = "https://preinstall.blob.core.windows.net/script/Azure%20VPN%20Client%20Installer.exe?sp=r&st=2025-04-30T10:42:04Z&se=2025-04-30T18:42:04Z&spr=https&sv=2024-11-04&sr=b&sig=kVCP4UddJPLJ6veHcusTODNlr5jPtDnWIV62cZpr7ls%3D"
+
+$localExePath = "$env:TEMP\AzureVpnClientInstaller.exe"
+
+Invoke-WebRequest -Uri $vpnExeUrl -OutFile $localExePath
+
+
+Start-Process -FilePath $localExePath -ArgumentList "/quiet", "/norestart" -Wait
